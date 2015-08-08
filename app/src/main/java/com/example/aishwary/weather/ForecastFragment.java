96c13ec251edbd77daac5ec20715efb61cf246f6
Aishwary Tandon <ashwarytandon@gmail.com>
2015-08-07 21:15:35 -0400

    Get Default location through shared preferences

--- app/src/main/java/com/example/aishwary/weather/ForecastFragment.java
+++ app/src/main/java/com/example/aishwary/weather/ForecastFragment.java
@@ -1,10 +1,13 @@
 package com.example.aishwary.weather;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.format.Time;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,6 +15,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -63,7 +67,11 @@ public class ForecastFragment extends Fragment {
         if (id == R.id.action_refresh) {
             //return true;
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("10003");
+            //weatherTask.execute("10003");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -101,6 +109,17 @@ public class ForecastFragment extends Fragment {
         //Get a reference to the listview and attach this adapter to it
         ListView listView = (ListView) rootView.findViewById(R.id.listView_forecast);
         listView.setAdapter(mForecastAdapter);
+        //Set on Click Listener on the listView
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                 String forecast = mForecastAdapter.getItem(position);
+                //Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                   startActivity(intent);
+            }
+        });
 
         return rootView;
     }
